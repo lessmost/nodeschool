265db01f63c2034e639220143141e5c68a9b10cb
zqlu <zqlu.cn@gmail.com>
2015-04-02 09:22:07 +0800

    EJS templating in Koa

---
+++ kickoffkoa/10_templating.js
@@ -0,0 +1,22 @@
+var koa = require('koa'),
+	views = require('co-views');
+
+var app = koa();
+var render = views(__dirname + '/views', {
+	ext: 'ejs'
+});
+
+var user = {
+	name: {
+		first: 'Tobi',
+		last: 'Holowaychuk'
+	},
+	species: 'ferret',
+	age: 3
+};
+
+app.use(function* () {
+	this.body = yield render('user', {user: user});
+});
+
+app.listen(process.argv[2] || 4000);
