b02b30a2b6e13f2ee135dc6a441c1b509e638d92
zqlu <zqlu.cn@gmail.com>
2015-05-14 09:39:10 +0000

    React.js and Server side rendering

---
+++ learnyoureact/02_Components.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use('/', function(req, res) {
+    res.render('components', '');
+});
+
+app.listen(app.get('port'), function() {});
---
+++ learnyoureact/07_PropsFromServer.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+var data = [
+    {
+        title: "Shopping",
+        detail: process.argv[3]
+    },
+    {
+        title: "Hair cut",
+        detail: process.argv[4]
+    }
+    ];
+
+app.use('/', function(req, res) {
+    res.render('propsFromServer', {data: data});
+});
+
+app.listen(app.get('port'), function() {});
