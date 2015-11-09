b830ea3f973015b7d62801187f3604c0841a8ca1
zqlu <zqlu.cn@gmail.com>
2015-11-09 07:07:48 +0000

    Export and render the Searchbar

--- thinking-in-react/1_FilterableProductTable.js
+++ thinking-in-react/1_FilterableProductTable.js
@@ -3,7 +3,21 @@ import React from 'react';
 export const FilterableProductTable = React.createClass({
     render() {
         return (
-            <div></div>
+            <SearchBar />
+            );
+    }
+});
+
+export const SearchBar = React.createClass({
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="Searching..." />
+                <p>
+                    <input type="checkbox" />
+                    Only show products in stock
+                </p>
+            </form>
             );
     }
 });
