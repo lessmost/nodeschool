cdeb0e2e70ab2000f2c3b99969dc63e754abe994
zqlu <zqlu.cn@gmail.com>
2015-11-09 08:11:08 +0000

    Static version of FilterProductTable

--- thinking-in-react/1_FilterableProductTable.js
+++ thinking-in-react/1_FilterableProductTable.js
@@ -3,7 +3,10 @@ import React from 'react';
 export const FilterableProductTable = React.createClass({
     render() {
         return (
-            <SearchBar />
+            <div>
+                <SearchBar />
+                <ProductTable products={this.props.products} />
+            </div>
             );
     }
 });
@@ -21,3 +24,57 @@ export const SearchBar = React.createClass({
             );
     }
 });
+
+export const ProductTable = React.createClass({
+    render() {
+        var rows = [];
+        var lastCategory = null;
+        this.props.products.forEach(function(product) {
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+            );
+    }
+});
+
+export const ProductCategoryRow = React.createClass({
+    render() {
+        return (
+            <tr>
+                <th>{this.props.category}</th>
+            </tr>
+            );
+    }
+});
+
+export const ProductRow = React.createClass({
+    render() {
+        var name = this.props.product.stocked ?
+        this.props.product.name :
+            <span style={{color: 'red'}}>
+                {this.props.product.name}
+            </span>;
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+            );
+    }
+});
+
